648bdb95a3fe7a8976e368661760171e6c1886f5
kurihada <63988604+kurihada@users.noreply.github.com>
2021-09-08 22:44:47 +0800

    create login forms

--- user/models.py
+++ user/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class UserProfile(AbstractUser):
-    mobile = models.CharField(max_length=11, verbose_name='手机号码', unique=True)
+    phone = models.CharField(max_length=11, verbose_name='手机号码', unique=True)
     avatar = models.ImageField(upload_to='uploads/%y/%m/d', default="")
 
     class Meta:
